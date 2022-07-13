80804e08ddf95674a93f8328efcf8438d30d2a4c
mdintezaralam <mintezar99@gmail.com>
2022-07-14 01:28:02 +0530

    sec

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <Link to="/">Shopping Cart</Link>
+          <a href="https://intnotes.netlify.app/">Shopping-App</a>
         </Navbar.Brand>
         {useLocation().pathname.split("/")[1] !== "cart" && (
           <Navbar.Text className="search">
