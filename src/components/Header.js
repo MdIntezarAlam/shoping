0d61642ada7028fc8c43d6642153b80fbe403a62
Intezar Alam <tanjavoorathefe@gmail.com>
2024-09-08 19:18:55 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
         <Navbar.Brand>
-          <a href="https://intezar-dev.netlify.app/">Shopping-App</a>
+          <a href="/">Shopping-App</a>
         </Navbar.Brand>
         {useLocation().pathname.split("/")[1] !== "cart" && (
           <Navbar.Text className="search">
